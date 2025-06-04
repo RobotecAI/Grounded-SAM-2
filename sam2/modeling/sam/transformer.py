b542676ee877c05174788db0896e76743701d355
Kacper Dąbrowski <kacper.dabrowski@robotec.ai>
2025-06-04 15:58:07 +0200

    Apply patch to transformer.py
    Signed-off-by: Kacper Dąbrowski <kacper.dabrowski@robotec.ai>

--- sam2/modeling/sam/transformer.py
+++ sam2/modeling/sam/transformer.py
@@ -21,6 +21,11 @@ from sam2.utils.misc import get_sdpa_settings
 warnings.simplefilter(action="ignore", category=FutureWarning)
 OLD_GPU, USE_FLASH_ATTN, MATH_KERNEL_ON = get_sdpa_settings()
 
+# https://github.com/facebookresearch/sam2/issues/48#issuecomment-2261822485
+USE_FLASH_ATTN = False
+MATH_KERNEL_ON = True
+OLD_GPU = True
+
 
 class TwoWayTransformer(nn.Module):
     def __init__(
